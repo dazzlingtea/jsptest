e3f6926c8d918ad451e731b03ec073330b54dad8
dazzlingtea <kitsnr08@gmail.com>
2024-05-11 18:39:13 +0900

    jdbc db connection

---
+++ src/main/java/com/jsp/chap5/JdbcBasic.java
@@ -0,0 +1,8 @@
+package com.jsp.chap5;
+
+public class JdbcBasic {
+    private String username = "root";
+    private String password = "mariadb";
+    private String url = "jdbc:mariadb://localhost:3306/spring5";
+    private String driverClassName = "org.mariadb.jdbc.Driver";
+}
